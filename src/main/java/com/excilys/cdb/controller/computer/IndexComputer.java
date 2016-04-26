9a2975ed62fdc05361d14f34b595a18ede23c09a
Camille Vrod <cvrod@excilys.com>
2016-04-26 18:43:29 +0200

    feat(IndexComputer): adding index range verification to index servlet

--- src/main/java/com/excilys/cdb/controller/computer/IndexComputer.java
+++ src/main/java/com/excilys/cdb/controller/computer/IndexComputer.java
@@ -15,19 +15,21 @@ import com.excilys.cdb.model.Computer;
 import com.excilys.cdb.pagination.Page;
 import com.excilys.cdb.service.ComputerService;
 
-/**.
- * Servlet implementation class IndexComputer
+/**
+ * . Servlet implementation class IndexComputer
  */
-@WebServlet(name = "IndexComputer",urlPatterns = {"/computer"})
+@WebServlet(name = "IndexComputer", urlPatterns = { "/computer" })
 public class IndexComputer extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	static ComputerService computerService = null;
-	static int index = 0;
+    private static final long serialVersionUID = 1L;
+
+    static ComputerService computerService = null;
+    static int index = 0;
     public static final int PAGE_OFFSET = 20;
     static final Logger LOGGER = LoggerFactory.getLogger(IndexComputer.class);
-       
-    /**.
+
+    /**
+     * .
+     * 
      * @see HttpServlet#HttpServlet()
      */
     public IndexComputer() {
@@ -35,36 +37,53 @@ public class IndexComputer extends HttpServlet {
         computerService = ComputerService.getInstance();
     }
 
-	/**.
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    String paramIndex = request.getParameter("index");
-	    if(paramIndex != null){
-	        try{
-	            index = Integer.parseInt(paramIndex);
-	            LOGGER.info("getting index : " + index);
-	        }catch(NumberFormatException e){
-	            LOGGER.debug("NumberFormatException on index param !");
-	            index = 0;
-	        }
-	    }
-	    Page<Computer> computerPage = computerService.listAllByPage(index, PAGE_OFFSET);
-	    Long countComputer = computerService.count();
-	    
-	    request.setAttribute("page", computerPage);
-	    request.setAttribute("index", index);
-	    request.setAttribute("countComputer", countComputer);
-	    request.setAttribute("offset", PAGE_OFFSET);
+    /**
+     * .
+     * 
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        String paramIndex = request.getParameter("index");
+        if (paramIndex != null) {
+            try {
+                index = Integer.parseInt(paramIndex);
+                LOGGER.info("getting index : " + index);
+            } catch (NumberFormatException e) {
+                LOGGER.debug("NumberFormatException on index param !");
+                index = 0;
+            }
+        }
+        Long countComputer = computerService.count();
 
-	    request.getRequestDispatcher("/WEB-INF/views/indexComputer.jsp").forward(request, response);
-	}
+        if (index < 0) {
+            index = 0;
+        } else if (index >= countComputer) {
+            index = (int) (countComputer - PAGE_OFFSET);
+        }
 
-	/**.
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        Page<Computer> computerPage = computerService.listAllByPage(index,
+                PAGE_OFFSET);
+
+        request.setAttribute("page", computerPage);
+        request.setAttribute("index", index);
+        request.setAttribute("countComputer", countComputer);
+        request.setAttribute("offset", PAGE_OFFSET);
+
+        request.getRequestDispatcher("/WEB-INF/views/indexComputer.jsp")
+                .forward(request, response);
+    }
+
+    /**
+     * .
+     * 
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
 }
