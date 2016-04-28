80816486f01a8d9f8ee42c0e4e77a4b19608f3e3
Camille Vrod <cvrod@excilys.com>
2016-04-28 18:05:02 +0200

    feat(EditComputer): adding EditComputer jsp & Servlet

--- src/main/java/com/excilys/cdb/controller/computer/EditComputer.java
+++ src/main/java/com/excilys/cdb/controller/computer/EditComputer.java
@@ -1,11 +1,23 @@
 package com.excilys.cdb.controller.computer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.excilys.cdb.dao.DAOException;
+import com.excilys.cdb.dto.model.ComputerDTO;
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.model.Computer;
+import com.excilys.cdb.service.CompanyService;
+import com.excilys.cdb.service.ComputerService;
 
 /**.
  * . Servlet implementation class EditComputer
@@ -13,13 +25,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "EditComputer", urlPatterns = { "/computer/edit" })
 public class EditComputer extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
+    ComputerService computerService = null;
+    CompanyService companyService = null;
+    static final Logger LOGGER = LoggerFactory.getLogger(EditComputer.class);
+    ArrayList<Company> companies = null;
+    
     /**.
      * EditComputer Servlet constructor
      */
     public EditComputer() {
         super();
-        // TODO Auto-generated constructor stub
+        computerService = ComputerService.getInstance();
+        companyService = CompanyService.getInstance();
     }
 
     /**.
@@ -32,7 +49,38 @@ public class EditComputer extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/views/editComputer.html")
+
+        String paramId = request.getParameter("id");
+        int id = 0;
+        LOGGER.debug("Try to retrieve Computer of id(str) : "+paramId);
+        ComputerDTO computerDTO = null;
+        Computer computer = null;
+
+        if (paramId != null) {
+            try {
+                id = Integer.parseInt(paramId);
+                LOGGER.info("getting Id : " + id);
+            } catch (NumberFormatException e) {
+                LOGGER.debug("NumberFormatException on id param !");
+            }
+        }
+        try {
+            companies = companyService.listAll();
+            computer = computerService.get(id);
+            LOGGER.debug("getting computer : "+computer.toString());
+            computerDTO = new ComputerDTO(computer);
+            LOGGER.debug("Getting CompanyService.listAll()");
+            
+        } catch (DAOException e) {
+            LOGGER.debug("Can't get companies list ! or computer");
+            companies = new ArrayList<>();
+        }
+
+        request.setAttribute("companies", companies);
+        request.setAttribute("id", id);
+        request.setAttribute("computer", computerDTO);
+
+        request.getRequestDispatcher("/WEB-INF/views/editComputer.jsp")
                 .forward(request, response);
     }
 
@@ -46,7 +94,6 @@ public class EditComputer extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
 }
