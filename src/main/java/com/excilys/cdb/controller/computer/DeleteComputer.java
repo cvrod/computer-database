c47382c56be5e0af65a2455dc7a2272b644b9694
Camille Vrod <cvrod@excilys.com>
2016-05-02 18:13:16 +0200

    feat(DeleteComputer): DeleteComputer Servlet WIP

---
+++ src/main/java/com/excilys/cdb/controller/computer/DeleteComputer.java
@@ -0,0 +1,48 @@
+package com.excilys.cdb.controller.computer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.excilys.cdb.service.ComputerService;
+
+/**
+ * Servlet implementation class DeleteComputer
+ */
+@WebServlet(name = "DeleteComputer", urlPatterns = { "/computer/delete" })
+public class DeleteComputer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ComputerService computerService = null;
+	static final Logger LOGGER = LoggerFactory.getLogger(DeleteComputer.class);
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteComputer() {
+        super();
+        this.computerService = ComputerService.getInstance();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    final String selection = request.getParameter("selection");
+	    LOGGER.debug("doPost()");
+	    LOGGER.debug("received : " + selection);
+	    
+	}
+
+}
