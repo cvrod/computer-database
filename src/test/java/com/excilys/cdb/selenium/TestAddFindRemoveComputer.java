b72377f0bbdcb4b63a42866e549709127818d02f
Camille Vrod <cvrod@excilys.com>
2016-05-04 14:14:31 +0200

    feat(Selenium): changing selenium test timeout

--- src/test/java/com/excilys/cdb/selenium/TestAddFindRemoveComputer.java
+++ src/test/java/com/excilys/cdb/selenium/TestAddFindRemoveComputer.java
@@ -17,7 +17,7 @@ public class TestAddFindRemoveComputer {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "http://localhost:8080/";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
