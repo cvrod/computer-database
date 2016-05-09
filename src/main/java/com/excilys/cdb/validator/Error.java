81339e00b2238f423ee44c7dbf8465f232be8f9d
Camille Vrod <cvrod@excilys.com>
2016-05-09 17:04:09 +0200

    feat(Error): adding Error object to handle error in validator

---
+++ src/main/java/com/excilys/cdb/validator/Error.java
@@ -0,0 +1,38 @@
+package com.excilys.cdb.validator;
+
+public class Error {
+
+    private String field;
+    private String message;
+
+    /**.
+     * Error constructor
+     * @param field form field concerned
+     * @param message error message
+     */
+    public Error(String field, String message) {
+        this.setField(field);
+        this.setMessage(message);
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Error [field=" + field + ", message=" + message + "]";
+    }
+}
